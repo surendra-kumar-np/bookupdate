c6ecc8454b306f73d230c2a20600774b96e7c541
surendra-kumar-np <surendra.kumar@netprophetsglobal.com>
2025-06-12 12:30:19 +0530

    clean

--- save.php
+++
@@ -1,4 +0,0 @@
-<?php
-$data = json_decode(file_get_contents("php://input"), true);
-file_put_contents('data.json', json_encode($data, JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE));
-echo "Data saved successfully.";
